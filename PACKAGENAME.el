0fc84f5e613f6ffc3b72e4cac2d516fb30c1e3dc
Jason Plein <jplein@adobe.com>
2020-09-26 13:04:22 -0700

    Adding PACKAGENAME.el

---
+++ PACKAGENAME.el
@@ -0,0 +1,23 @@
+;;; PACKAGENAME.el -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2020 Jason Plein
+;;
+;; Author: John Doe <http://github/jplein>
+;; Maintainer: John Doe <john@doe.com>
+;; Created: September 26, 2020
+;; Modified: September 26, 2020
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/jplein/jpp-themes
+;; Package-Requires: ((emacs 26.3) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+(provide 'jpp-themes)
+;;; PACKAGENAME.el ends here
